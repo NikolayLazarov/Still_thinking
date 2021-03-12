30bfb379cb8d9248b42a45eb27dd886bedcf031d
JIOjosBG <jjjooosssiiifff@gmail.com>
2021-03-12 21:01:57 +0200

    add new pages

--- project/pages/views.py
+++ project/pages/views.py
@@ -2,3 +2,35 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def list(request):
+
+    context={
+        "items":[
+            {
+                "title":"New",
+                "description":"this is description",
+                "usluga":"prodajba/naem/podaryk"
+            },
+            {
+                "title":"New",
+                "description":"this is description",
+                "usluga":"prodajba/naem/podaryk"
+            },
+            {
+                "title":"New",
+                "description":"this is description",
+                "usluga":"prodajba/naem/podaryk"
+            },
+            {
+                "title":"New",
+                "description":"this is description",
+                "usluga":"prodajba/naem/podaryk"
+            },
+        ]
+
+    }
+    return render(request, "list.html",context)
