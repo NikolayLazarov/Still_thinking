62f168a2a7a84d2aa4ce8f372e089851f840ebdd
Danny <danail.yordanov1@gmail.com>
2021-03-13 12:30:16 +0200

    Delete django app

--- project/project/settings.py
+++ project/project/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'main.apps.MainConfig',
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
 ]
