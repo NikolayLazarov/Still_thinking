5178bba9a8ead12ac0b8e4698370c3fb5f84c221
Danny <danail.yordanov1@gmail.com>
2021-03-13 12:46:13 +0200

    Add djangorestframework and update the requirements.txt

--- project/project/urls.py
+++ project/project/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('pages.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
