d3276a7295d9e2de167761b04a4e325e0aa66356
Danail Yordanov <danail.yordanov1@gmail.com>
2021-03-14 05:46:57 +0200

    Add rest api endpoint (item detail)

--- project/api/urls.py
+++ project/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('items/', views.ItemListAPIView.as_view(), name='api-item-list')
+    path('items/', views.ItemListAPIView.as_view(), name='api-item-list'),
+    path('item/<int:pk>/', views.ItemRetrieveAPIView.as_view(), name='api-item-detail')
 ]
--- project/api/views.py
+++ project/api/views.py
@@ -1,7 +1,11 @@
 from rest_framework import generics
-from .searilzers import ItemSerializer
+from .searilzers import ItemImageSerializer, ItemSerializer
 from pages.models import Item
 
 class ItemListAPIView(generics.ListAPIView):
+    serializer_class = ItemSerializer
+    queryset = Item.objects.all()
+
+class ItemRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = ItemSerializer
     queryset = Item.objects.all()
