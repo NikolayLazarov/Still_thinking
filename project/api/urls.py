890cb5f29aa302fe0f3fee8ec9ed4ffb90e36e50
Danail Yordanov <danail.yordanov1@gmail.com>
2021-03-14 06:01:55 +0200

    Add new api endpoint (item delete)

--- project/api/urls.py
+++ project/api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('items/', views.ItemListAPIView.as_view(), name='api-item-list'),
-    path('item/<int:pk>/', views.ItemRetrieveAPIView.as_view(), name='api-item-detail')
+    path('item/<int:pk>/', views.ItemRetrieveAPIView.as_view(), name='api-item-detail'),
+    path('item/delete/<int:pk>/', views.ItemDestroyAPIView.as_view(), name='api-item-delete')
 ]
--- project/api/views.py
+++ project/api/views.py
@@ -5,7 +5,13 @@ from pages.models import Item
 class ItemListAPIView(generics.ListAPIView):
     serializer_class = ItemSerializer
     queryset = Item.objects.all()
+    
 
 class ItemRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = ItemSerializer
+    queryset = Item.objects.all()
+
+
+class ItemDestroyAPIView(generics.DestroyAPIView):
     serializer_class = ItemSerializer
     queryset = Item.objects.all()
